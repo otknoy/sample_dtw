ea7b391e4fadd5ba73366e1132b45b1e339dad0f
otknoy <otknoy@gmail.com>
2015-09-27 19:45:39 +0900

    Sample

--- src/test.js
+++ src/test.js
@@ -1,19 +1,25 @@
 var DTW = require('dtw');
 
 
-var ts = [[1, 2, 3],
-	  [1, 2, 2.9],
-	  [1, 1.8, 2.6, 3],
-	  [2, 3, 4]];
+var ts = [[1, 2, 3, 4, 5],
+	  [1, 2, 3, 4, 4],
+	  [1, 3, 5],
+	  [5, 5, 5, 5, 5]];
+
 
 var dtw = new DTW();
 
-for (var i = 1; i < ts.length; i++) {
+for (var i = 0; i < ts.length; i++) {
     var cost = dtw.compute(ts[0], ts[i]);
     var path = dtw.path();
 
+    console.log("\n");
+    console.log(ts[0]);
+    console.log(ts[i]);
+    console.log(cost);
+    console.log(path);
+
     var div = document.createElement('div');
-    
 
     var ts0 = document.createTextNode(ts[0] + '\n');
     var tsi = document.createTextNode(ts[i] + '\n');
@@ -25,5 +31,3 @@ for (var i = 1; i < ts.length; i++) {
     document.body.appendChild(c);
     document.body.appendChild(p);
 }
-
-
